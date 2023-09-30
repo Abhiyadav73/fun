8c3ec99700b55fdc08e6b0a87c041e24db09e89d
Abhiyadav73 <124858761+Abhiyadav73@users.noreply.github.com>
2023-09-30 16:59:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function something(){
+list=["Bsdk","Bkl","MC","BC","Dobara click kar bsdk"]
+const randomNumber1=Math.floor(Math.random()*list.length);
+
+document.querySelector("h1").textContent=list[randomNumber1]
+}
